81502690ab5e29a9b3c82b62266d932a953d857d
Pierre Beaujeu <pierre.beaujeu@rednet.io>
2015-01-20 20:40:06 +0100

    test(user-model,#21): added tests for duplicates and authenticate()
    I have added test for duplicate username and password. I have also
    written 2 tests for the authenticate() method.

--- test/user-model.test.js
+++ test/user-model.test.js
@@ -41,7 +41,7 @@ describe('user#model', function () {
     });
   });
 
-  it('should not duplicate username', function (done) {
+  it('should not duplicate email', function (done) {
     var data = {
       username: 'test',
       email: 'test1@test.test',
@@ -52,7 +52,7 @@ describe('user#model', function () {
 
     data.email = 'test2@test.test';
 
-    var u2 = new User(data);
+    var u2 = new User(JSON.parse(JSON.stringify(data)));
 
     u1.save(function (err, u1) {
       if (err) {
@@ -65,6 +65,63 @@ describe('user#model', function () {
 
         done();
       });
+    });
+  });
+
+  it('should not duplicate username', function (done) {
+    var data = {
+      username: 'test1',
+      email: 'test@test.test',
+      password: 'test'
+    };
+
+    var u1 = new User(data);
+
+    data.username = 'test2';
+
+    var u2 = new User(JSON.parse(JSON.stringify(data)));
+
+    u1.save(function (err, u1) {
+      if (err) {
+        throw err;
+      }
+
+      u2.save(function (err, u2) {
+        expect(err).to.exist();
+        expect(u2).to.not.exist();
+
+        done();
+      });
+    });
+  });
+
+  describe('authenticate()', function () {
+    var user;
+
+    beforeEach(function (done) {
+      user = new User({
+        username: 'test',
+        email: 'test@test.test',
+        password: 'test'
+      });
+
+      user.save(done);
+    });
+
+    afterEach(function (done) {
+      User.remove({}, done);
+    });
+
+    it('should return true if password match', function () {
+      var res = user.authenticate('test');
+
+      expect(res).to.be.equal(true);
+    });
+
+    it('should return false if password mismatch', function () {
+      var res = user.authenticate('false');
+
+      expect(res).to.be.equal(false);
     });
   });
 });
